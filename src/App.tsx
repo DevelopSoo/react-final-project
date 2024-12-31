978bf6248909f5662bdfeac1e8977449fd6aa977
DevelopSoo <69490984+DevelopSoo@users.noreply.github.com>
2024-12-31 16:11:25 +0900

    초기세팅

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import supabase from "./utils/supabase"
+
+console.log(supabase)
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
